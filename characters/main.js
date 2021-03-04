ea57e1ee3dfb193c6864a271b2b0b182ccc39e00
Thor Anderson <ta.anderson@gmail.com>
2021-03-04 12:42:59 -0700

    Added eventListener to maleButton

--- characters/main.js
+++ characters/main.js
@@ -2,19 +2,40 @@ import { people } from '../data/people.js'
 
 const mainContent = document.querySelector('#main')
 
-people.forEach(person => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    let charNum = getLastNumber(person.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    const charCaption = document.createElement('figcaption')
+const mainHeader = document.createElement('header')
 
-    charCaption.textContent = person.name
-
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-    mainContent.appendChild(charFigure)
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => {
+    populateDOM(otherCharacters)
 })
+
+mainHeader.appendChild(maleButton)
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' || person.gender === 'none') {
+        return person
+    }
+})
+
+function populateDOM(characters) {
+    characters.forEach(person => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(person.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        const charCaption = document.createElement('figcaption')
+
+        charCaption.textContent = person.name
+
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+        mainContent.appendChild(charFigure)
+    })
+}
 
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
