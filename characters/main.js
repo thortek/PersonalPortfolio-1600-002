d659fab322d236ad3ca74ba9b1db1147385e2d7e
Thor Anderson <ta.anderson@gmail.com>
2021-03-04 12:22:44 -0700

    Updated getLastNumber to handle 1 or 2 digits

--- characters/main.js
+++ characters/main.js
@@ -1,1 +1,26 @@
-console.log("Welcome to my characters page")
+import { people } from '../data/people.js'
+
+const mainContent = document.querySelector('#main')
+
+people.forEach(person => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    let charNum = getLastNumber(person.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    const charCaption = document.createElement('figcaption')
+
+    charCaption.textContent = person.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+})
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
