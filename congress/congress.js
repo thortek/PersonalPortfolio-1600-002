e63e6ea3f5a4b8c89da77bce85b14ba741d0e0df
Thor Anderson <ta.anderson@gmail.com>
2021-04-13 12:12:03 -0600

    Added icons

--- congress/congress.js
+++ congress/congress.js
@@ -5,6 +5,11 @@ import { removeChildren } from '../utils/index.js'
 const congressGrid = document.querySelector('.congressGrid')
 const seniorityButton = document.querySelector('#seniorityButton')
 const birthdayButton = document.querySelector('#birthdayButton')
+const republicansButton = document.querySelector('#republicans')
+
+republicansButton.addEventListener('click', () => {
+    populateCongressDiv(filterCongressPeople(representatives, 'R'))
+})
 
 seniorityButton.addEventListener('click', () => senioritySort())
 
@@ -16,10 +21,14 @@ function populateCongressDiv(simplifiedList) {
         let personFig = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
+        let partyIcon = document.createElement('i')
+        if (person.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (person.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (person.party === 'ID') partyIcon.className = 'fas fa-mitten'
 
         figImg.src = person.imgURL
         figCaption.textContent = person.name
-
+        figCaption.appendChild(partyIcon)
         personFig.appendChild(figImg)
         personFig.appendChild(figCaption)
         personDiv.appendChild(personFig)
@@ -34,13 +43,18 @@ function getSimplifiedPeople(peopleList) {
             id: person.id,
             name: `${person.first_name}${middleName} ${person.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
-            seniority: parseInt(person.seniority, 10)
+            seniority: parseInt(person.seniority, 10),
+            party: person.party
         }
     })
 }
 
 function senioritySort() {
     populateCongressDiv(getSimplifiedPeople(senators).sort((a, b) => a.seniority - b.seniority).reverse())
+}
+
+const filterCongressPeople = (chamber, politicalParty) => {
+    return getSimplifiedPeople(chamber).filter(member => member.party === politicalParty)
 }
 
 populateCongressDiv(getSimplifiedPeople(senators))
