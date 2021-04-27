cff5e333595302f527b647c773c5c74d555a7831
Thor Anderson <ta.anderson@gmail.com>
2021-04-27 12:26:14 -0600

    Can we fetch from Netlify?

--- Marvel/marvel.js
+++ Marvel/marvel.js
@@ -1,24 +1,16 @@
 const main = document.querySelector('main')
 
-async function getAPIData(url) {
-  try {
-    const response = await fetch(url) // try getting data from the API at the url provided
-    const data = await response.json() // convert the response into JSON
-    return data // return the data from the function to whoever called it
-  } catch (error) {
-    // must have been an error
-    console.log(error)
-    alert('Could not find that data')
-  }
-}
 
-function loadPage() {
-  getAPIData(
-    `https://gateway.marvel.com:443/v1/public/characters?apikey=d09a3b298e51e26ea519b4d5b519f8b7`,
-  ).then(async (data) => {
-    console.log(data)
-  })
-}
-
-loadPage()
-
+fetch("https://aerisweather1.p.rapidapi.com/alerts/jordan,mt", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "f4810bc1e1msh8e5f44dd0f55ddfp107b05jsnc1c5efa46815",
+		"x-rapidapi-host": "aerisweather1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
